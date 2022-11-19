134d5fb6911078ef1e2cfb536eeaf7c1edac0562
pk910 <philipp@pk910.de>
2022-11-19 23:55:58 +0100

    cache go modules in github workflow

--- scripts/build-genesis.sh
+++ scripts/build-genesis.sh
@@ -6,11 +6,12 @@ setup_apps(){
     set -x
     cd ./apps
 
+    go install github.com/protolambda/eth2-val-tools@latest
+
     if ! [ -d "./eth2-testnet-genesis" ]; then
         git clone https://github.com/pk910/eth2-testnet-genesis.git
         cd eth2-testnet-genesis
         go install .
-        go install github.com/protolambda/eth2-val-tools@latest
         cd ..
     fi
 
